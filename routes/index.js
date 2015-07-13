fcc388e6dba9583cdc21a88d3bec01a8be9d6046
Manuel Antonio Fernández <m.antonio.fernandez@ricoh.es>
2015-07-13 21:34:47 +0200

    Primera version de quizes

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
+});
+
+/* GET question. */
+router.get('/quizes/question', quizController.question);
+
+
+/* GET answer. */
+router.get('/quizes/answer', quizController.answer);
+
+router.get('/author', quizController.author);
+
+module.exports = router;
